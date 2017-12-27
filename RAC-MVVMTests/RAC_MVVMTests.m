64ac30d7ed61e64aae4255e5d9aa963a73ec0579
wenhua <xudacheng_tomous@163.com>
2017-12-27 10:33:41 +0800

    Initial Commit

---
+++ RAC-MVVMTests/RAC_MVVMTests.m
@@ -0,0 +1,39 @@
+//
+//  RAC_MVVMTests.m
+//  RAC-MVVMTests
+//
+//  Created by wenhua on 2017/12/27.
+//  Copyright © 2017年 wenhua. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RAC_MVVMTests : XCTestCase
+
+@end
+
+@implementation RAC_MVVMTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
